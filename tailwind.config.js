2ed40d3b1ce6634e13957cfa2bce59ac9198fdfa
aditya379 <at37907@gmail.com>
2024-02-06 08:33:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwindcss -i ./css/input.css -o ./css/output.css --watch
